346f99b7c6861c188b9fdc1d3bce3ab685a7a6e3
e753gh <Harry.Singh@ehi.com>
2021-08-21 16:30:28 -0500

    JPA Enumeration

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -2,6 +2,8 @@ package guru.springframework.domain;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -24,7 +26,9 @@ public class Recipe {
   private String source;
   private String url;
   private String directions;
-  //TODO: add difficulty
+
+  @Enumerated(value = EnumType.STRING)
+  private Difficulty difficulty;
 
   @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
   //mappedBy property is the property on the on the Ingredient class
@@ -122,5 +126,13 @@ public class Recipe {
 
   public void setIngredients(Set<Ingredient> ingredients) {
     this.ingredients = ingredients;
+  }
+
+  public Difficulty getDifficulty() {
+    return difficulty;
+  }
+
+  public void setDifficulty(Difficulty difficulty) {
+    this.difficulty = difficulty;
   }
 }
